fa7660dbf6a189f5f296e59c8860f5a56a351aec
gaurav chakarwarti <136673555+Gauravchakarwarti@users.noreply.github.com>
2023-06-15 13:22:18 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+# Write a Python program that accepts a word from the user and reverse it
+# Sample Test Case
+
+
+
+# Input : Edyoda
+
+# output: adoydE
+
+# solution:
+
+word = input("Enter a word:")
+reversed_word = word[::-1]
+print("reversed_word:",reversed_word)
+
+word = "Edyoda"
+reversed_word = word[::-1]
+print("reversed_word:", reversed_word)
